f9fa98255d68f6ade11298282615802064f2ddef
Mohamed <84293568+mohamedbtt9@users.noreply.github.com>
2024-04-11 15:22:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 
-app = Flask(__name__) #creating flask app name #com1
+app = Flask(__name__) #creating flask app name #com12
 
 @app.route('/')
 def home():
